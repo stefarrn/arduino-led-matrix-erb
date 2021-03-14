d9e2e44aa5d984c263c59550befa05c67acb18c3
Timon Lilje <timon.lilje@gmail.com>
2021-03-14 18:07:58 +0100

    first draft

--- web/java.js
+++ web/java.js
@@ -1,30 +1,38 @@
-let led = [];
-let scl = 100;
+let col = [false, false, false, false, false];
+let row = [false, false, false, false, false, false, false];
+let scl = 80;
 
 function setup() {
-  createCanvas(4 * scl, scl);
+  createCanvas(5 * scl, 7 * scl);
   frameRate(2);
-  getLedStatus();
+  getMatrixStatus();
 }
 
 function draw() {
   background(200);
-  getLedStatus();
+  getMatrixStatus();
   frameRate(2);
 
-  for (var i = 0; i < led.length; i++) {
-    col = led[i] ? [200, 60, 30] : [40, 40, 40]
-    fill(col)
-    strokeWeight(2)
-    rect(i * scl, 0, scl, scl)
+  for (let x = 0; x < col.length; x++) {
+    for (let y = 0; y < row.length; y++) {
+      let state = col[x] && row[y];
+      let color = state ? [200, 0, 0] : [50, 50, 50];
+
+      fill(color)
+      rect(x * scl, y * scl, scl, scl)
+    }
   }
 }
 
 function mousePressed() {
-  if (mouseY > scl) {
-    return false;
-  } else {
-    x = Math.floor(mouseX / scl)
-    changeLedStatus(x)
-  }
+  let updateCol = [...col];
+  let updateRow = [...row];
+
+  let x = Math.floor(mouseX/scl);
+  let y = Math.floor(mouseY/scl);
+
+  updateCol[x] = !updateCol[x];
+  updateRow[y] = !updateRow[y];
+
+  setMatrixStatus(updateCol, updateRow);
 }
